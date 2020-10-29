8b083f232440d6ef57e56810452d0da4fe1bc1b5
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-29 22:28:52 +0300

    Scripted auto-commit on change (2020-10-29 22:28:52) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/AiroportIdWritable.java
+++ src/main/java/ru/bmstu/ui9/lab2/AiroportIdWritable.java
@@ -1,7 +1,23 @@
 package ru.bmstu.ui9.lab2;
 
-public class AiroportIdWritable {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 
-    
+import java.io.DataInput;
+import java.io.DataOutput;
 
+public class AiroportIdWritable implements Writable  {
+
+    Text
+
+
+    @Override
+    public void write(DataOutput output){
+
+    }
+
+    @Override
+    public void readFields(DataInput input){
+
+    }
 }
