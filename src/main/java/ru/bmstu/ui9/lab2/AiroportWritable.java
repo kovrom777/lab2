1b0800c505c9cfd29d6da9d0ac15f87ae1c3bed6
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-29 20:33:42 +0300

    Scripted auto-commit on change (2020-10-29 20:33:42) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/AiroportWritable.java
+++ src/main/java/ru/bmstu/ui9/lab2/AiroportWritable.java
@@ -1,7 +1,11 @@
 package ru.bmstu.ui9.lab2;
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Writable;
 
 public class AiroportWritable implements Writable {
+
+
+    IntWritable 
 
 }
