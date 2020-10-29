92eff106a2f53813e9d3d85ba727f4c599c8696b
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-29 22:10:59 +0300

    Scripted auto-commit on change (2020-10-29 22:10:59) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/MainClass.java
+++ src/main/java/ru/bmstu/ui9/lab2/MainClass.java
@@ -15,12 +15,13 @@ public class MainClass {
             System.exit(-1);
         }
         Job job = Job.getInstance();
-        job.setJarByClass(WordCountApp.class);
-        job.setJobName("Word count");
+        job.setJarByClass(MainClass.class);
+        job.setJobName("MainClass");
         FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setMapperClass(WordMapper.class);
-        job.setReducerClass(WordReducer.class);
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setMapperClass(MainClass.class);
+        job.setReducerClass(MainClass.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         job.setNumReduceTasks(2);
