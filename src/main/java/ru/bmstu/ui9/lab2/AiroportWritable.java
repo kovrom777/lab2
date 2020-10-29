ade767fa6d9d9994c819aa9da3d3b5b3940e2797
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-29 20:45:34 +0300

    Scripted auto-commit on change (2020-10-29 20:45:34) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/AiroportWritable.java
+++ src/main/java/ru/bmstu/ui9/lab2/AiroportWritable.java
@@ -2,7 +2,11 @@ package ru.bmstu.ui9.lab2;
 
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
 
 public class AiroportWritable implements Writable {
 
@@ -10,5 +14,21 @@ public class AiroportWritable implements Writable {
     IntWritable year, quarter, month, day_of_month, day_of_week, airline_ID, fl_num, origin_airport_id, origin_airport_seq_id, origin_city_market_id,
     dest_airport_id, wheels_on, arr_time;
 
-    DoubleWritable arr_delay, getArr_delay_new, canceled, air_time, distance
+    DoubleWritable arr_delay, getArr_delay_new, canceled, air_time, distance;
+
+    Text fl_date, unique_carrier, carrier, tail_num, cancelation_code;
+
+
+
+
+
+    @Override
+    public void write(DataOutput output){
+
+    }
+
+    @Override
+    public void readFields(DataInput input){
+
+    }
 }
