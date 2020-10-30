44e855724866417c9e323ed219e1e9765ab7c285
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-30 10:47:52 +0300

    Scripted auto-commit on change (2020-10-30 10:47:52) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/AirportWritableComparable.java
+++ src/main/java/ru/bmstu/ui9/lab2/AirportWritableComparable.java
@@ -1,4 +1,26 @@
 package ru.bmstu.ui9.lab2;
 
-public class AirportWritableComparable {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+
+public class AirportWritableComparable implements WritableComparable {
+
+
+    @Override
+    public void write(DataOutput output){
+
+    }
+
+    @Override
+    public void readFields(DataInput input){
+
+    }
+
+    @Override
+    public void compareTo(AirportWritableComparable comp){
+
+    }
+
 }
