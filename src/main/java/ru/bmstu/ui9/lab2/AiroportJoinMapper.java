96186ec74c3883973fcb12bd3b68578a8bb3d6c9
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-30 10:42:14 +0300

    Scripted auto-commit on change (2020-10-30 10:42:14) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/AiroportJoinMapper.java
+++ src/main/java/ru/bmstu/ui9/lab2/AiroportJoinMapper.java
@@ -1,8 +1,10 @@
 package ru.bmstu.ui9.lab2;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AiroportJoinMapper extends Mapper {
+public class AiroportJoinMapper extends Mapper <LongWritable, Text, > {
 
 
 
