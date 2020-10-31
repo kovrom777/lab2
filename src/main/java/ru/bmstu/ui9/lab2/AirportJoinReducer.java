768ef455f03036cc4f6530ded77b93d23293568f
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-31 10:35:23 +0300

    Scripted auto-commit on change (2020-10-31 10:35:23) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/AirportJoinReducer.java
+++ src/main/java/ru/bmstu/ui9/lab2/AirportJoinReducer.java
@@ -1,6 +1,20 @@
 package ru.bmstu.ui9.lab2;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class AirportJoinReducer extends Reducer {
+import java.util.Iterator;
+
+
+public class AirportJoinReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
+
+    @Override
+    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context contex){
+        Iterator<Text> iterator = values.iterator();
+        Text airport = new Text(iterator.next());
+        if (iterator.hasNext()){
+            
+        }
+    }
+
 }
