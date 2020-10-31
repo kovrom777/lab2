e2bfdccb0b9e3a786de6535995f5ce5ae3da6f68
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-31 13:37:21 +0300

    Scripted auto-commit on change (2020-10-31 13:37:20) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/AiroportsComparableClass.java
+++ src/main/java/ru/bmstu/ui9/lab2/AiroportsComparableClass.java
@@ -1,11 +1,19 @@
 package ru.bmstu.ui9.lab2;
 
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 
-public class AiroportsComparableClass extends WritableComparable {
+public class AiroportsComparableClass extends WritableComparator {
 
     protected AiroportsComparableClass(){
-        super(AirportWritableComparable.class, true);
+//        super(AirportWritableComparable.class, true);
+
+        @Override
+        public int compare(WritableComparable comparator){
+
+        }
     }
+
+
 
 }
