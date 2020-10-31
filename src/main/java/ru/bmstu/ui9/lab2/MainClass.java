04b6cb0400a4b11d35e84a2dccfedfa566aa382e
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-31 13:59:28 +0300

    Scripted auto-commit on change (2020-10-31 13:59:28) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/MainClass.java
+++ src/main/java/ru/bmstu/ui9/lab2/MainClass.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -21,10 +20,16 @@ public class MainClass {
         job.setJobName("MainClass");
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        job.setMapperClass(MainClass.class);
-        job.setReducerClass(MainClass.class);
+
+        job.setMapperClass(AirportJoinMapper.class);
+        job.setPartitionerClass(AirportJoinPartioner.class);
+        job.setMapOutputKeyClass(AirportWritableComparable.class);
+        job.setReducerClass(AirportJoinReducer.class);
+
+
+
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
