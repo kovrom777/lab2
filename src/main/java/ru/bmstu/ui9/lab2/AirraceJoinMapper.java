a11cf0a6ced20dcc01d4fe4f4f155e02750ede5f
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-31 14:08:39 +0300

    Scripted auto-commit on change (2020-10-31 14:08:39) by gitwatch.sh

--- src/main/java/ru/bmstu/ui9/lab2/AirraceJoinMapper.java
+++ src/main/java/ru/bmstu/ui9/lab2/AirraceJoinMapper.java
@@ -4,9 +4,11 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirraceJoinMapper extends Mapper<LongWritable, Text, AiroportsComparableClass, Text> {
 
-    protected void map (LongWritable key, Text value, Mapper.Context context){
+    protected void map (LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String lineNumber = value.toString();
         String[] flyInfoArray = lineNumber.split(",");
 
@@ -14,7 +16,9 @@ public class AirraceJoinMapper extends Mapper<LongWritable, Text, AiroportsCompa
             if (flyInfoArray[14].equals("") && !flyInfoArray[17].equals("")){
                 int airportId = Integer.parseInt(flyInfoArray[14]);
                 double delayTime = Double.parseDouble(flyInfoArray[17]);
-
+                if (delayTime > 0){
+                    context.write(new AirportWritableComparable(airportId, 1), new Text("DelayTime: " + delayTime));
+                }
             }
         }
 
